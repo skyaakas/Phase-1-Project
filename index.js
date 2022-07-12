8fdc0d42f895b70f0d05ccc0ac8d1daa324d6f09
Michael Dear <michaelcdear@gmail.com>
2022-07-12 12:41:53 -0600

    update to main

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const contentContainer = document.getElementById("contentContainer")
+const popUpMenu = document.createElement("div")
 
 document.addEventListener("DOMContentLoaded", () => {
     fetch("https://api.punkapi.com/v2/beers")
@@ -18,5 +19,40 @@ function renderData (data) {
         image.className = "image"
         image.src = array[index].image_url
         imgContainer.append(image)
+
+        const title = document.createElement("h2")
+        title.className = "title"
+        title.textContent = array[index].name
+        imgContainer.append(title)
     })
 }
+
+document.body.addEventListener("click", event => {
+    if (event.target.className === "entry" || event.target.className === "image") {
+        console.log("iht")
+
+    }
+})
+
+const button = document.querySelector(".modal-btn")
+const newsletter = document.querySelector(".modal-bg")
+const modalClose = document.querySelector(".modal-close")
+
+button.addEventListener("click", function() {
+    newsletter.classList.add("bg-active")
+})
+
+modalClose.addEventListener("click", function() {  
+    newsletter.classList.remove("bg-active")
+})
+
+//For animation purposes
+var hotbod = document.querySelector("body");
+
+function doStuff() {
+    hotbod.className += " animate";
+}
+
+window.onload = function() {
+    doStuff();
+};
