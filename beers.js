5640b4b1bced061a03702c09e9b6065e1c02728f
Michael Dear <michaelcdear@gmail.com>
2022-07-13 12:31:11 -0600

    populated data with errors

--- beers.js
+++ beers.js
@@ -1,29 +1,162 @@
-// document.body.addEventListener("click", event => {
-//     fetch("https://api.punkapi.com/v2/beers")
-//     .then(resp => resp.json())
-//     .then(data => populateJSON(data[0]))
-// })
+document.addEventListener("click", event => {
+    fetch("https://api.punkapi.com/v2/beers")
+    .then(resp => resp.json())
+    .then(data => populateData(data))
+})
 
-// function populateJSON(JSON) {
-//     fetch("http://localhost:3000/beers", {
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify(JSON)
-// })
-// }
+function populateData(data) {
 
-// document.body.addEventListener("click", event => {
-//     let beerData = {
-//         name: "Summer Ale",
-//         abv: "7.0%"
-//     }
-//     fetch("http://localhost:3000/beers", {
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify(beerData)
-// })
-// })
+    console.log("...Populated")
+
+    // fetch("http://localhost:3000/beers", {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(data[1])
+    // })
+
+    // fetch("http://localhost:3000/beers", {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(data[3])
+    // })
+
+    // fetch("http://localhost:3000/beers", {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(data[4])
+    // })
+
+    // fetch("http://localhost:3000/beers", {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(data[5])
+    // })
+
+    // fetch("http://localhost:3000/beers", {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(data[6])
+    // })
+
+    // fetch("http://localhost:3000/beers", {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(data[7])
+    // })
+
+    // fetch("http://localhost:3000/beers", {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(data[8])
+    // })
+
+    // fetch("http://localhost:3000/beers", {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(data[9])
+    // })
+
+    // fetch("http://localhost:3000/beers", {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(data[11])
+    // })
+
+    // fetch("http://localhost:3000/beers", {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(data[12])
+    // })
+
+    // fetch("http://localhost:3000/beers", {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(data[13])
+    // })
+
+    // fetch("http://localhost:3000/beers", {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(data[14])
+    // })
+
+    // fetch("http://localhost:3000/beers", {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(data[15])
+    // })
+
+    // fetch("http://localhost:3000/beers", {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(data[16])
+    // })
+
+    // fetch("http://localhost:3000/beers", {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(data[17])
+    // })
+
+    // fetch("http://localhost:3000/beers", {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(data[18])
+    // })
+
+    // fetch("http://localhost:3000/beers", {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(data[21])
+    // })
+
+    // fetch("http://localhost:3000/beers", {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(data[22])
+    // })
+
+    fetch("http://localhost:3000/beers", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data[24])
+    })
+}
