adc0997a9d1c1ef903609fd3f524b47efa14a19d
gabrielb5 <gabrielburgosmm@gmail.com>
2022-07-13 09:48:39 -0700

    small change

--- beers.js
+++ beers.js
@@ -1,18 +1,18 @@
-document.body.addEventListener("click", event => {
-    fetch("https://api.punkapi.com/v2/beers")
-    .then(resp => resp.json())
-    .then(data => populateJSON(data[0]))
-})
+// document.body.addEventListener("click", event => {
+//     fetch("https://api.punkapi.com/v2/beers")
+//     .then(resp => resp.json())
+//     .then(data => populateJSON(data[0]))
+// })
 
-function populateJSON(JSON) {
-    fetch("http://localhost:3000/beers", {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(JSON)
-})
-}
+// function populateJSON(JSON) {
+//     fetch("http://localhost:3000/beers", {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(JSON)
+// })
+// }
 
 // document.body.addEventListener("click", event => {
 //     let beerData = {
