46644bf263aac1ecb7ca88d6c84bd944c41cfcde
Michael Dear <michaelcdear@gmail.com>
2022-07-13 16:52:21 -0600

    jdf

--- src/index.js
+++ src/index.js
@@ -87,29 +87,3 @@ function handleRenderSearch(){
 document.querySelector('#search').addEventListener('click', handleRenderSearch)
 
 // document.querySelector("#home").addEventListener("click", window.location.reload())
-
-
-
-
-
-
-// pop up beer info 
-
-const beerInfo = document.createElement("div")
-
-document.addEventListener("click", event => {
-    beerInfo.className = "beerInfo"
-
-    if(event.target.className === "entry") {
-        contentContainer.append(beerInfo)
-        beerInfo.style.left = event.target.getBoundingClientRect().left + window.scrollX + "px"
-        beerInfo.style.top = event.target.getBoundingClientRect().top + window.scrollY + "px"
-
-        console.log("Target: ", event.target)
-        console.log("pos.x: ", event.target.getBoundingClientRect().left + window.scrollX)
-        console.log("pos.y: ", event.target.getBoundingClientRect().top + window.scrollY)
-        
-    } else {
-        beerInfo.remove()
-    }
-})
