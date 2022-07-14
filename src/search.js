f918d3340a9615439648b32a7f7c82340fcd6b31
Michael Dear <michaelcdear@gmail.com>
2022-07-14 09:57:44 -0600

    removed variable redeclarations

--- src/search.js
+++ src/search.js
@@ -110,23 +110,3 @@ function handleRenderSearch(){
 document.querySelector('#search').addEventListener('click', handleRenderSearch)
 
 // document.querySelector("#home").addEventListener("click", window.location.reload())
-
-
-const beerInfo = document.createElement("div")
-
-// document.addEventListener("click", event => {
-//     beerInfo.className = "beerInfo"
-//     if(event.target.className === "entry") {
-//         contentContainer.append(beerInfo)
-//         beerInfo.style.left = event.target.getBoundingClientRect().left + window.scrollX + "px"
-//         beerInfo.style.top = event.target.getBoundingClientRect().top + window.scrollY + "px"
-
-//         console.log("Target: ", event.target)
-//         console.log("pos.x: ", event.target.getBoundingClientRect().left + window.scrollX)
-//         console.log("pos.y: ", event.target.getBoundingClientRect().top + window.scrollY)
-        
-//     } else {
-//         beerInfo.remove()
-//         // console.log("Removed")
-//     }
-// })
