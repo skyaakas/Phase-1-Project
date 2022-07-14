3ef232e7238de8b82d8229492022f8e2b642feee
Michael Dear <michaelcdear@gmail.com>
2022-07-14 11:41:38 -0600

    added left reflected tab

--- src/beerinfo.js
+++ src/beerinfo.js
@@ -9,6 +9,13 @@ document.addEventListener("click", event => {
             beerInfo.removeChild(beerInfo.firstChild)
         }
 
+        console.log(`x.pos ${event.target.getBoundingClientRect().left + window.scrollX}`)
+        console.log(`y.pos ${event.target.getBoundingClientRect().top + window.scrollY}`)
+
+        const xRange = event.target.getBoundingClientRect().left + window.scrollX
+
+        if(xRange < 800) {
+
         contentContainer.append(beerInfo)
         beerInfo.style.left = event.target.getBoundingClientRect().left + window.scrollX + "px"
         beerInfo.style.top = event.target.getBoundingClientRect().top + window.scrollY + "px"
@@ -24,8 +31,7 @@ document.addEventListener("click", event => {
         beerInfo.append(beerPic)
 
         const beerInfoList = document.createElement("ul")
-        beerInfo.append(beerInfoList)
-
+        beerTabTitle.append(beerInfoList)
 
         const brewDate = document.createElement("li")
         brewDate.textContent = beerData[event.target.id - 1].first_brewed
@@ -52,28 +58,69 @@ document.addEventListener("click", event => {
         beerFoodPair.className = "beerFoodPair"
         beerInfo.append(beerFoodPair)
 
+        }
 
+        console.log("Width:")
+        console.log(event.target.offsetWidth)
 
-        // beerData[event.target.id].name
+        if(xRange > 800) {
 
-
-
-        // for(let element of entryArry){
-        //     element.style.zIndex = 0
-        // }
-
-        // event.target.style.zIndex = 3
-        // event.target.style.backgroundColor = "lightgrey"
-
-        // event.target.style.zIndex = 3
-        // event.target.style.backgroundColor = "white"
+            contentContainer.append(beerInfo)
+            beerInfo.style.left = event.target.getBoundingClientRect().left + window.scrollX - 800 + event.target.offsetWidth + "px"
+            beerInfo.style.top = event.target.getBoundingClientRect().top + window.scrollY + "px"
+    
+            const beerTabTitle = document.createElement("h1")
+            beerTabTitle.className = "beerTabTitle"
+            beerTabTitle.textContent = beerData[event.target.id - 1].name
+            beerInfo.append(beerTabTitle)
+            beerTabTitle.style.float = "left"
+            beerTabTitle.style.marginLeft = "13%"
+           //beerTabTitle.style.
+    
+            const beerPic = document.createElement("img")
+            beerPic.src = beerData[event.target.id - 1].image_url
+            beerPic.className = "beerPic"
+            beerInfo.append(beerPic)
+            beerPic.style.float = "right"
+            beerPic.style.marginRight = "10%"
+            beerPic.style.marginLeft = "0%"
+    
+            const beerInfoList = document.createElement("ul")
+            beerInfoList.className = "beerInfoList"
+            beerTabTitle.append(beerInfoList)
+    
+            const brewDate = document.createElement("li")
+            brewDate.textContent = beerData[event.target.id - 1].first_brewed
+            brewDate.className = "brewDate"
+            beerInfoList.append(brewDate)
+    
+            const beerTagline = document.createElement("li")
+            beerTagline.textContent = beerData[event.target.id - 1].tagline
+            beerTagline.className = "beerTagline"
+            beerInfoList.append(beerTagline)
+    
+            const beerABV = document.createElement("li")
+            beerABV.textContent = beerData[event.target.id - 1].abv
+            beerABV.className = "beerABV"
+            beerInfoList.append(beerABV)
+    
+            const beerDescription = document.createElement("p")
+            beerDescription.textContent = beerData[event.target.id - 1].description
+            beerDescription.className = "beerDescription"
+            beerInfo.append(beerDescription)
+            beerDescription.style.marginLeft = "5%"
+    
+            const beerFoodPair = document.createElement("p")
+            beerFoodPair.textContent = beerData[event.target.id - 1].food_pairing
+            beerFoodPair.className = "beerFoodPair"
+            beerInfo.append(beerFoodPair)
+            beerFoodPair.style.marginLeft = "5%"
+    
+            }
         
     } else {
+
         beerInfo.remove()
-        // for(let element of entryArry){
-        //     element.style.zIndex = 1
-        //     element.style.backgroundColor = "white"
-        //     element.children[0].style.zIndex = -1
-        // }
+
     }
 })
