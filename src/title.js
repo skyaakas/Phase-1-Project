30050b5a35946a729a711ec3eeb191007d4ede9c
Michael Dear <michaelcdear@gmail.com>
2022-07-14 14:41:55 -0600

    new file

---
+++ src/title.js
@@ -0,0 +1,17 @@
+let hotbod = document.querySelector("body");
+
+function doStuff() {
+    hotbod.className += " animate";
+}
+
+window.onload = function() {
+    doStuff();
+};
+
+function findsBeer() {
+    beerSearchForm.addEventListener("submit", event => {
+        event.preventDefault()
+        alert(beerInput.value)
+        searchForm.reset()
+    })
+}
