be4e8942c07f915fad6b4a7d17c8e3d8ebf38a06
Michael Dear <michaelcdear@gmail.com>
2022-07-13 18:20:22 -0600

    playing around

--- src/beerinfo.js
+++ src/beerinfo.js
@@ -4,23 +4,32 @@ const entryArry = document.getElementsByClassName("entry")
 document.addEventListener("click", event => {
     beerInfo.className = "beerInfo"
 
+    console.log("Target:")
+    console.log(event.target)
+    console.log("Child:")
+    console.log(event.target.children[0])
+
     if(event.target.className === "entry") {
         contentContainer.append(beerInfo)
         beerInfo.style.left = event.target.getBoundingClientRect().left + window.scrollX + "px"
         beerInfo.style.top = event.target.getBoundingClientRect().top + window.scrollY + "px"
 
-        console.log(entryArry)
+        // for(let element of entryArry){
+        //     element.style.zIndex = 0
+        // }
 
-        entryArry.forEach(element => element.style.zIndex = -1)
+        // event.target.style.zIndex = 3
+        // event.target.style.backgroundColor = "lightgrey"
 
-        event.target.style.zIndex = 3
-        event.target.style.backgroundColor = "white"
-
-        console.log("Target: ", event.target)
-        console.log("pos.x: ", event.target.getBoundingClientRect().left + window.scrollX)
-        console.log("pos.y: ", event.target.getBoundingClientRect().top + window.scrollY)
+        // event.target.style.zIndex = 3
+        // event.target.style.backgroundColor = "white"
         
     } else {
         beerInfo.remove()
+        // for(let element of entryArry){
+        //     element.style.zIndex = 1
+        //     element.style.backgroundColor = "white"
+        //     element.children[0].style.zIndex = -1
+        // }
     }
 })
