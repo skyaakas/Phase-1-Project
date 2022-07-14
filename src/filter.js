71645e2d75c5bc64e170feb44a2120d88f618d28
gabrielb5 <gabrielburgosmm@gmail.com>
2022-07-14 11:46:19 -0700

    new filter

---
+++ src/filter.js
@@ -0,0 +1,65 @@
+// function filter(collection, cb) {
+//     const newCollection = [];
+  
+//     for (const beer of collection) {
+//       if (cb(beer)) {
+//         newCollection.push(beer);
+//       }
+//     }
+  
+//     return newCollection;
+//   }
+
+// const filterTypeCb = filter(collection,function (input) {
+//     return beer.type === input;
+// });
+
+// const filterAbvCb = filter(collection,function (input) {
+//     return beer.abv === input;
+// });
+const filterContainer = document.getElementById("filter-content-container")
+function filterData (data) {
+    console.log(data)
+    beerData = data
+
+    data.forEach((element, index, array) => {
+
+        const imgContainer = document.createElement("div")
+        imgContainer.className = "entry"
+        filterContainer.append(imgContainer)
+        imgContainer.id = element.id
+
+        const image = document.createElement("img")
+        image.className = "image"
+        image.src = array[index].image_url
+        imgContainer.append(image)
+
+        const title = document.createElement("h2")
+        title.className = "title"
+        title.textContent = array[index].name
+        imgContainer.append(title)
+    })
+}
+
+function grabCategory(event) {
+    const alcohol = event.target.textContent
+    console.log(alcohol)
+    filterCategory(alcohol)
+    contentContainer.style.display = "none"
+
+}
+
+
+function filterCategory(alcohol) {
+    fetch("http://localhost:3000/beers")
+    .then(res => res.json())
+    .then(beers => {
+        const result = beers.filter(beer => beer.type == alcohol)
+        filterData(result)
+        // console.log(result)
+    }
+)}
+
+
+
+
